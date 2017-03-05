2307944a2c1f71d846a0c0806716634ed2daf5b4
FranPregernik <franpregernik+github@goglemail.com>
2017-03-05 05:31:11 +0100

    Add logging framework
    Add logback, slf4j and jul bridge (tornadofx)

--- designer/src/main/kotlin/hr/franp/rsim/Main.kt
+++ designer/src/main/kotlin/hr/franp/rsim/Main.kt
@@ -2,11 +2,19 @@ package hr.franp.rsim
 
 import javafx.stage.*
 import org.controlsfx.glyphfont.*
+import org.slf4j.bridge.*
 import tornadofx.*
 
 class Main : App(DesignerView::class, Styles::class) {
 
     init {
+        // Optionally remove existing handlers attached to j.u.l root logger
+        SLF4JBridgeHandler.removeHandlersForRootLogger()  // (since SLF4J 1.6.5)
+
+        // add SLF4JBridgeHandler to j.u.l's root logger, should be done once during
+        // the initialization phase of your application
+        SLF4JBridgeHandler.install()
+
         reloadStylesheetsOnFocus()
 
         val faStream = Main::class.java.getResourceAsStream("/fontawesome-webfont.ttf")
